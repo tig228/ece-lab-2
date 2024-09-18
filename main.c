fea9832a48faf60df74cfa24a446769c14311304
tig228 <tig228@lehigh.edu>
2024-09-18 16:46:06 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
- /*
- * GPIO_Driver.h
- *
- *  Created on: Sep 2, 2024
- *      Author: naher
+#include <msp430.h> 
+
+
+/**
+ * main.c
  */
-
-#ifndef GPIO_DRIVER_H_
-#define GPIO_DRIVER_H_
-void gpioInitIn(unsigned int Port, unsigned char Pin);
-void gpioInitOut(unsigned int Port, unsigned char Pin);
-unsigned char gpioInitStatus(unsigned int Port, unsigned char Pin);
-void gpioWrite(unsigned int Port, unsigned char Pin, unsigned char value);
-
-#endif /* GPIO_DRIVER_H_ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	return 0;
+}
