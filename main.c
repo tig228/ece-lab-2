1b6303245906647d2a5503a0c5935b238535e57a
tig228 <tig228@lehigh.edu>
2024-09-18 16:43:39 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,49 +1,15 @@
-#include <msp430.h>
-#define RED_LED BIT0 //P1.0
-#define GREEN_LED BIT6 //P6.6
-#define BUTTON1 BIT1 //push button P4.1
-#define BUTTON2 BIT3 //push button P2.3
+ /*
+ * GPIO_Driver.h
+ *
+ *  Created on: Sep 2, 2024
+ *      Author: naher
+ */
 
+#ifndef GPIO_DRIVER_H_
+#define GPIO_DRIVER_H_
+void gpioInitIn(unsigned int Port, unsigned char Pin);
+void gpioInitOut(unsigned int Port, unsigned char Pin);
+unsigned char gpioInitStatus(unsigned int Port, unsigned char Pin);
+void gpioWrite(unsigned int Port, unsigned char Pin, unsigned char value);
 
-
-void main(void)
-{
-WDTCTL = WDTPW | WDTHOLD;
-P1OUT &= ~BIT0;  //p1.0 red led
-P6OUT &= ~BIT6; //p6.6 green led
-P1DIR |= BIT0;
-P6DIR |= BIT6;
-
-
-P4DIR &= ~BIT1;//clear P4.1(s1)
-P4REN |= BIT1;//Enable pull up/down resistor
-P4OUT |= BIT1;//Make resistor a pull up
-
-P2DIR &= ~BIT3;//clear P2.3(s2)
-P2REN |= BIT3;//Enable pull up/down resistor
-P2OUT |= BIT3;//Make resistor a pull up
-
-
-
-PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                           // to activate previously configured port settings
-int count = 0;
-while(1){
-        if((P4IN & BUTTON1) == 0x00){
-        _delay_cycles(5000);
-        if((P4IN & BUTTON1) == 0x00){
-            P1OUT ^= RED_LED;
-            } while((P4IN & BUTTON1) == 0x00);
-        }
-
-        else if((P2IN & BUTTON2) == 0x00){
-         _delay_cycles(5000);
-        if((P2IN & BUTTON2) == 0x00){
-            P6OUT ^= GREEN_LED;
-            }
-        while((P2IN & BUTTON2) == 0x00);}
-
-
-}
-return 0;
-}
+#endif /* GPIO_DRIVER_H_ */
